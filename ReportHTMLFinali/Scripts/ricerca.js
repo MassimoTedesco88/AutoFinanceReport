0e30dfcb9d2d19fde9c09ce6127faa3266308ec8
Martin Pescatore <martinp@localhost.localdomain>
2025-07-26 08:46:08 +0200

    Auto publish reports

---
+++ ReportHTMLFinali/Scripts/ricerca.js
@@ -0,0 +1,36 @@
+document.getElementById("tickerForm").addEventListener("submit",async function(e){
+	e.preventDefault();
+	const ticker = document.getElementById("ticker").value.trim().toUpperCase();
+	console.log(ticker);
+	const resultsBox = document.getElementById("results");
+	resultsBox.innerHTML = "Loading ....";
+	try {
+		const response= await fetch('https://analisi-finale-ticker.vercel.app/api/cerca_ticker?ticker=${ticker}');
+		if(!response.ok) throw new Error("API request failed");
+		const data = await response.json();
+		if(Array.isArray(data) && data.lenght >0){
+			let table = "<table><thead><tr>";
+			//create table header
+			for (let key in data[0]){
+				table += '<th>${key}</th>';
+			}
+			table+="</tr></thead><tbody>";
+			//create rows
+			data.forEach(row => {
+				table+= "<tr>";
+				for (let key in row) {
+					table += '<td>${row[key]}</td>';
+				}
+				table+="</tr>";
+			});
+			table+="</tbody></table>";
+			resultsBox.innerHTML = table;
+		} else {
+			resultsBox.innerHTML = '<p>Dati non trovati per <strong>${ticker}</strong>.</p>';
+		}
+	}
+	catch (error) {
+		console.error(error);
+		resultsBox.innerHTML ='<p><i>Errore nella ricerca dei Dati </i></p>';
+	}
+});
