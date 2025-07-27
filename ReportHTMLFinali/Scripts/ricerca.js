f4a563d574d7d6619d2fc7ac4a9f638d5637a816
Martin Pescatore <martinp@localhost.localdomain>
2025-07-27 07:37:11 +0200

    Auto publish reports

--- ReportHTMLFinali/Scripts/ricerca.js
+++ ReportHTMLFinali/Scripts/ricerca.js
@@ -5,7 +5,7 @@ document.getElementById("tickerForm").addEventListener("submit",async function(e
 	const resultsBox = document.getElementById("results");
 	
 	resultsBox.innerHTML = "Loading ....";
-	const urlBase="https://analisi-finale-ticker.vercel.app/api/cerca_ticker?ticker=$";// encodeURIComponent() trasforma una stringa in formato sicuro da usare in URL
+	const urlBase='https://analisi-finale-ticker.vercel.app/api/cerca_ticker?ticker=$';// encodeURIComponent() trasforma una stringa in formato sicuro da usare in URL
 	const url=${urlBase}${ticker};//unione tra due stringhe
 	console.log(urlBase);//debug
 	console.log(url);//debug
