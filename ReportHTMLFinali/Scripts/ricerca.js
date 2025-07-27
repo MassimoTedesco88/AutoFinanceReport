8fe8d51af2fb7ae5f444a40e8bd6f6854957343b
Martin Pescatore <martinp@localhost.localdomain>
2025-07-27 08:40:51 +0200

    Auto publish reports

--- ReportHTMLFinali/Scripts/ricerca.js
+++ ReportHTMLFinali/Scripts/ricerca.js
@@ -14,32 +14,16 @@ document.getElementById("tickerForm").addEventListener("submit",async function(e
 		const response= await fetch(url);//chiama l'API su Vercel passando il nome come paramentro nella URL
 		
 		if(!response.ok) throw new Error("API request failed");
-		const data = await response.json();//converte la risposta in formato JSON
+		const data = await response.json();//converte la risposta in formato JSON		
 		console.log(data);//debug
-		if(Array.isArray(data) && data.lenght >0){ //
-			let table = "<table><thead><tr>";
-			//create table header
-			for (let key in data[0]){
-				table += '<th>${key}</th>';
-				console.log(table);//debug
-			}
-			table+="</tr></thead><tbody>";
-			//create rows
-			data.forEach(row => {
-				table+= "<tr>";
-				for (let key in row) {
-					table += '<td>${row[key]}</td>';
-					console.log(table);//debug
-				}
-				table+="</tr>";
-			});
-			table+="</tbody></table>";
-			resultsBox.innerHTML = table;
-			console.log(table);//debug
-			console.log(resultsBox);//debug
-		} else {
-			resultsBox.innerHTML = '<p>Dati non trovati per <strong>'+ticker+'</strong></p>';
-		}
+		
+		rows.forEach((row,i) => {
+			const p = document.createElement('p');
+			//costruisci stringa con colonne separate da ,
+			const rowText= Object.entries(row).map(([key,value]) => '${key}: ${value}').join(', ');
+			p.textContent = rowText;
+			resultsBox.appendChild(p);});		
+		
 	}
 	catch (error) {
 		console.error(error);
